fec8d017b20a19637a68c2b892f42af254ee56d0
Randall Degges <rdegges@gmail.com>
2012-08-05 20:09:11 -0700

    Adding a `heroku` decorator.
    This decorator simply ensures that the wrapped view is *only* accessible by
    Heroku's origin servers.
    
    It does this by checking the basic auth credentials of the user making the
    request, and comparing them to the username / password that you are expecting,
    stored via environment variables.

--- samurai/decorators.py
+++ samurai/decorators.py
@@ -1,1 +1,34 @@
 """Useful decorators and stuff."""
+
+
+from functools import wraps
+from os import environ
+
+from basicauth import DecodeError, decode
+from flask import abort, request
+
+
+def heroku(f):
+    """Validate that all incoming requests are coming from Heroku.
+
+    If any request is invalid (can't prove it is the Heroku origin server),
+    raise a 401.
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        authorization = request.headers.get('AUTHORIZATION')
+        if not authorization:
+            abort(401)
+
+        try:
+            username, password = decode(request.headers['AUTHORIZATION'])
+        except DecodeError:
+            abort(401)
+
+        if username != environ.get('SAMURAI_USERNAME') or password != environ.get('SAMURAI_PASSWORD'):
+            abort(401)
+
+        return f(*args, **kwargs)
+
+    return decorated_function
